62811b84da847ca9f6bc7d5bad04bf52647eb88f
emost22 <emoney96@naver.com>
2023-08-11 00:51:53 +0900

    Feat: add save, update experience api

---
+++ src/main/java/com/sirius/spurt/service/controller/experience/ExperienceController.java
@@ -0,0 +1,44 @@
+package com.sirius.spurt.service.controller.experience;
+
+import com.sirius.spurt.common.resolver.user.LoginUser;
+import com.sirius.spurt.service.business.experience.SaveExperienceBusiness;
+import com.sirius.spurt.service.business.experience.UpdateExperienceBusiness;
+import com.sirius.spurt.service.controller.RestResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/experience")
+@RequiredArgsConstructor
+public class ExperienceController {
+    private final SaveExperienceBusiness saveExperienceBusiness;
+    private final UpdateExperienceBusiness updateExperienceBusiness;
+
+    /**
+     * @param dto
+     * @return
+     * @title 나의 경험 저장 api
+     */
+    @PostMapping
+    public RestResponse<SaveExperienceBusiness.Result> saveExperience(
+            LoginUser loginUser, @RequestBody SaveExperienceBusiness.Dto dto) {
+        dto.setUserId(loginUser.getUserId());
+        return RestResponse.success(saveExperienceBusiness.execute(dto));
+    }
+
+    /**
+     * @param dto
+     * @return
+     * @title 나의 경험 수정 api
+     */
+    @PutMapping
+    public RestResponse<UpdateExperienceBusiness.Result> updateExperience(
+            LoginUser loginUser, @RequestBody UpdateExperienceBusiness.Dto dto) {
+        dto.setUserId(loginUser.getUserId());
+        return RestResponse.success(updateExperienceBusiness.execute(dto));
+    }
+}
