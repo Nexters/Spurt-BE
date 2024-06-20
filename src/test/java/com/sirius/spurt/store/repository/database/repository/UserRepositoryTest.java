5593ff5f6edb3aac2d5d3b96808f7cf7c2e5c717
emost22 <emoney96@naver.com>
2024-06-20 23:55:54 +0900

    Test: add exists user

---
+++ src/test/java/com/sirius/spurt/store/repository/database/repository/UserRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.sirius.spurt.store.repository.database.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.sirius.spurt.common.config.QuerydslConfigTest;
+import com.sirius.spurt.test.UserTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Import(QuerydslConfigTest.class)
+class UserRepositoryTest implements UserTest {
+    @Autowired private UserRepository userRepository;
+
+    @Test
+    void 유저_존재_확인_테스트() {
+        // given
+        userRepository.save(TEST_USER);
+
+        // when
+        boolean isExistsUser = userRepository.existsByUserId(TEST_USER_ID);
+
+        // then
+        assertThat(isExistsUser).isEqualTo(true);
+    }
+}
