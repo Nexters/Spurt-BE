eab8ea6faa77e5449df953867d9df6970ea4b61c
emost22 <emoney96@naver.com>
2024-06-21 01:07:11 +0900

    Test: add ExperienceTest

---
+++ src/test/java/com/sirius/spurt/test/ExperienceTest.java
@@ -0,0 +1,24 @@
+package com.sirius.spurt.test;
+
+import com.sirius.spurt.store.repository.database.entity.ExperienceEntity;
+import java.sql.Timestamp;
+
+public interface ExperienceTest extends UserTest {
+    Long TEST_EXPERIENCE_ID = 1L;
+    String TEST_EXPERIENCE_TITLE = "title";
+    String TEST_EXPERIENCE_CONTENT = "content";
+    Timestamp TEST_EXPERIENCE_START_DATE = Timestamp.valueOf("2023-07-01 00:00:00");
+    Timestamp TEST_EXPERIENCE_END_DATE = Timestamp.valueOf("2023-08-19 00:00:00");
+    String TEST_EXPERIENCE_LINK = "link";
+
+    ExperienceEntity TEST_EXPERIENCE =
+            ExperienceEntity.builder()
+                    .experienceId(TEST_EXPERIENCE_ID)
+                    .title(TEST_EXPERIENCE_TITLE)
+                    .content(TEST_EXPERIENCE_CONTENT)
+                    .startDate(TEST_EXPERIENCE_START_DATE)
+                    .endDate(TEST_EXPERIENCE_END_DATE)
+                    .link(TEST_EXPERIENCE_LINK)
+                    .userEntity(TEST_USER)
+                    .build();
+}
