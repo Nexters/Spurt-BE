dac366d6f8b6b02c56a271a9688b057131e6e5a2
emost22 <emoney96@naver.com>
2024-06-21 00:22:08 +0900

    Test: add find user

--- src/test/java/com/sirius/spurt/store/repository/database/repository/UserRepositoryTest.java
+++ src/test/java/com/sirius/spurt/store/repository/database/repository/UserRepositoryTest.java
@@ -3,6 +3,7 @@ package com.sirius.spurt.store.repository.database.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.sirius.spurt.common.config.QuerydslConfigTest;
+import com.sirius.spurt.store.repository.database.entity.UserEntity;
 import com.sirius.spurt.test.UserTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,5 +29,17 @@ class UserRepositoryTest implements UserTest {
 
         // then
         assertThat(isExistsUser).isEqualTo(true);
+    }
+
+    @Test
+    void 유저_조회_테스트() {
+        // given
+        UserEntity savedUser = userRepository.save(TEST_USER);
+
+        // when
+        UserEntity userEntity = userRepository.findByUserId(TEST_USER_ID);
+
+        // then
+        assertThat(userEntity).isEqualTo(savedUser);
     }
 }
