2dde9bfe967d7a80ae75794daeb87039d101042a
emost22 <emoney96@naver.com>
2024-06-21 01:37:57 +0900

    Test: add find question by question and user

--- src/test/java/com/sirius/spurt/store/repository/database/repository/QuestionRepositoryTest.java
+++ src/test/java/com/sirius/spurt/store/repository/database/repository/QuestionRepositoryTest.java
@@ -8,6 +8,7 @@ import com.sirius.spurt.store.repository.database.entity.QuestionEntity;
 import com.sirius.spurt.store.repository.database.entity.UserEntity;
 import com.sirius.spurt.test.ExperienceTest;
 import com.sirius.spurt.test.QuestionTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -24,12 +25,15 @@ class QuestionRepositoryTest implements QuestionTest, ExperienceTest {
     @Autowired private UserRepository userRepository;
     @Autowired private ExperienceRepository experienceRepository;
 
-    @Test
-    void 질문_조회_테스트() {
-        // given
-        UserEntity savedUser = userRepository.save(TEST_USER);
-        ExperienceEntity savedExperience = experienceRepository.save(TEST_EXPERIENCE);
-        QuestionEntity savedQuestion =
+    private UserEntity savedUser;
+    private ExperienceEntity savedExperience;
+    private QuestionEntity savedQuestion;
+
+    @BeforeEach
+    void init() {
+        savedUser = userRepository.save(TEST_USER);
+        savedExperience = experienceRepository.save(TEST_EXPERIENCE);
+        savedQuestion =
                 questionRepository.save(
                         QuestionEntity.builder()
                                 .questionId(TEST_QUESTION_ID)
@@ -41,10 +45,34 @@ class QuestionRepositoryTest implements QuestionTest, ExperienceTest {
                                 .pinUpdatedTime(TEST_PIN_UPDATED_TIME)
                                 .experienceId(savedExperience.getExperienceId())
                                 .build());
+    }
+
+    @Test
+    void 질문_조회_테스트() {
+        // given
 
         // when
         QuestionEntity questionEntity =
                 questionRepository.findByQuestionId(savedQuestion.getQuestionId());
+
+        // then
+        assertThat(questionEntity.getUserId()).isEqualTo(savedQuestion.getUserId());
+        assertThat(questionEntity.getSubject()).isEqualTo(savedQuestion.getSubject());
+        assertThat(questionEntity.getMainText()).isEqualTo(savedQuestion.getMainText());
+        assertThat(questionEntity.getJobGroup()).isEqualTo(savedQuestion.getJobGroup());
+        assertThat(questionEntity.getPinIndicator()).isEqualTo(savedQuestion.getPinIndicator());
+        assertThat(questionEntity.getPinUpdatedTime()).isEqualTo(savedQuestion.getPinUpdatedTime());
+        assertThat(questionEntity.getExperienceId()).isEqualTo(savedQuestion.getExperienceId());
+    }
+
+    @Test
+    void 질문_조회_question_user_테스트() {
+        // given
+
+        // when
+        QuestionEntity questionEntity =
+                questionRepository.findByQuestionIdAndUserId(
+                        savedQuestion.getQuestionId(), savedUser.getUserId());
 
         // then
         assertThat(questionEntity.getUserId()).isEqualTo(savedQuestion.getUserId());
