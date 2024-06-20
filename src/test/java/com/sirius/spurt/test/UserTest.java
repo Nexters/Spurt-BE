f1acfff940f542f28c492e2a4d165158bd963481
emost22 <emoney96@naver.com>
2024-06-20 23:55:11 +0900

    Test: add UserTest info

---
+++ src/test/java/com/sirius/spurt/test/UserTest.java
@@ -0,0 +1,21 @@
+package com.sirius.spurt.test;
+
+import com.sirius.spurt.common.meta.JobGroup;
+import com.sirius.spurt.store.repository.database.entity.UserEntity;
+
+public interface UserTest {
+    String TEST_USER_ID = "userId";
+    JobGroup TEST_JOB_GROUP = JobGroup.DEVELOPER;
+    String TEST_EMAIL = "email@gmail.com";
+    Boolean TEST_HAS_PINED = Boolean.TRUE;
+    Boolean TEST_HAS_POSTED = Boolean.TRUE;
+
+    UserEntity TEST_USER =
+            UserEntity.builder()
+                    .userId(TEST_USER_ID)
+                    .jobGroup(TEST_JOB_GROUP)
+                    .email(TEST_EMAIL)
+                    .hasPined(TEST_HAS_PINED)
+                    .hasPosted(TEST_HAS_POSTED)
+                    .build();
+}
