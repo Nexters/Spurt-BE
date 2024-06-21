ca10986dcbc91a02ee2158265f64e29e72cce396
emost22 <emoney96@naver.com>
2024-06-22 01:53:53 +0900

    Test: add save experience

---
+++ src/test/java/com/sirius/spurt/store/provider/experience/impl/ExperienceProviderImplTest.java
@@ -0,0 +1,124 @@
+package com.sirius.spurt.store.provider.experience.impl;
+
+import static com.sirius.spurt.common.meta.ResultCode.EXPERIENCE_THREE_SECONDS;
+import static com.sirius.spurt.common.meta.ResultCode.NOT_EXIST_USER;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.sirius.spurt.common.exception.GlobalException;
+import com.sirius.spurt.store.provider.experience.vo.ExperienceVo;
+import com.sirius.spurt.store.repository.database.entity.BaseEntity;
+import com.sirius.spurt.store.repository.database.repository.ExperienceRepository;
+import com.sirius.spurt.store.repository.database.repository.UserRepository;
+import com.sirius.spurt.test.ExperienceTest;
+import java.lang.reflect.Field;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ExperienceProviderImplTest implements ExperienceTest {
+    @InjectMocks private ExperienceProviderImpl experienceProvider;
+
+    @Mock private ExperienceRepository experienceRepository;
+    @Mock private UserRepository userRepository;
+
+    @Nested
+    class 경험_저장 {
+        @Test
+        void 경험_저장_성공_테스트() {
+            // given
+            when(userRepository.findByUserId(any())).thenReturn(TEST_USER);
+            when(experienceRepository.findTopByUserEntityOrderByCreateTimestampDesc(any()))
+                    .thenReturn(null);
+            when(experienceRepository.save(any())).thenReturn(TEST_EXPERIENCE);
+
+            // when
+            ExperienceVo experienceVo =
+                    experienceProvider.saveExperience(
+                            TEST_EXPERIENCE_TITLE,
+                            TEST_EXPERIENCE_CONTENT,
+                            TEST_EXPERIENCE_START_DATE_STRING,
+                            TEST_EXPERIENCE_END_DATE_STRING,
+                            TEST_EXPERIENCE_LINK,
+                            TEST_USER_ID);
+
+            // then
+            verify(userRepository).findByUserId(any());
+            verify(experienceRepository).findTopByUserEntityOrderByCreateTimestampDesc(any());
+            verify(experienceRepository).save(any());
+            assertThat(experienceVo.getTitle()).isEqualTo(TEST_EXPERIENCE_TITLE);
+            assertThat(experienceVo.getContent()).isEqualTo(TEST_EXPERIENCE_CONTENT);
+            assertThat(experienceVo.getStartDate()).isEqualTo(TEST_EXPERIENCE_START_DATE_STRING);
+            assertThat(experienceVo.getEndDate()).isEqualTo(TEST_EXPERIENCE_END_DATE_STRING);
+            assertThat(experienceVo.getLink()).isEqualTo(TEST_EXPERIENCE_LINK);
+        }
+
+        @Test
+        void 경험_저장_실패_테스트_유저없음() {
+            // given
+            when(userRepository.findByUserId(any())).thenReturn(null);
+
+            // when
+            GlobalException exception =
+                    assertThrows(
+                            GlobalException.class,
+                            () -> {
+                                experienceProvider.saveExperience(
+                                        TEST_EXPERIENCE_TITLE,
+                                        TEST_EXPERIENCE_CONTENT,
+                                        TEST_EXPERIENCE_START_DATE_STRING,
+                                        TEST_EXPERIENCE_END_DATE_STRING,
+                                        TEST_EXPERIENCE_LINK,
+                                        TEST_USER_ID);
+                            });
+
+            // then
+            verify(userRepository).findByUserId(any());
+            assertThat(exception.getResultCode()).isEqualTo(NOT_EXIST_USER);
+        }
+
+        @Test
+        void 경험_저장_실패_테스트_3초등록() {
+            // given
+            try {
+                Field field = BaseEntity.class.getDeclaredField("createTimestamp");
+                field.setAccessible(true);
+                field.set(TEST_EXPERIENCE, Timestamp.valueOf(LocalDateTime.now()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            when(userRepository.findByUserId(any())).thenReturn(TEST_USER);
+            when(experienceRepository.findTopByUserEntityOrderByCreateTimestampDesc(any()))
+                    .thenReturn(TEST_EXPERIENCE);
+
+            // when
+            GlobalException exception =
+                    assertThrows(
+                            GlobalException.class,
+                            () -> {
+                                experienceProvider.saveExperience(
+                                        TEST_EXPERIENCE_TITLE,
+                                        TEST_EXPERIENCE_CONTENT,
+                                        TEST_EXPERIENCE_START_DATE_STRING,
+                                        TEST_EXPERIENCE_END_DATE_STRING,
+                                        TEST_EXPERIENCE_LINK,
+                                        TEST_USER_ID);
+                            });
+
+            // then
+            verify(userRepository).findByUserId(any());
+            verify(experienceRepository).findTopByUserEntityOrderByCreateTimestampDesc(any());
+            assertThat(exception.getResultCode()).isEqualTo(EXPERIENCE_THREE_SECONDS);
+        }
+    }
+}
--- src/test/java/com/sirius/spurt/test/ExperienceTest.java
+++ src/test/java/com/sirius/spurt/test/ExperienceTest.java
@@ -2,13 +2,18 @@ package com.sirius.spurt.test;
 
 import com.sirius.spurt.store.repository.database.entity.ExperienceEntity;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
 public interface ExperienceTest extends UserTest {
     Long TEST_EXPERIENCE_ID = 1L;
     String TEST_EXPERIENCE_TITLE = "title";
     String TEST_EXPERIENCE_CONTENT = "content";
     Timestamp TEST_EXPERIENCE_START_DATE = Timestamp.valueOf("2023-07-01 00:00:00");
+    String TEST_EXPERIENCE_START_DATE_STRING =
+            new SimpleDateFormat("yyyy-MM").format(TEST_EXPERIENCE_START_DATE);
     Timestamp TEST_EXPERIENCE_END_DATE = Timestamp.valueOf("2023-08-19 00:00:00");
+    String TEST_EXPERIENCE_END_DATE_STRING =
+            new SimpleDateFormat("yyyy-MM").format(TEST_EXPERIENCE_END_DATE);
     String TEST_EXPERIENCE_LINK = "link";
 
     ExperienceEntity TEST_EXPERIENCE =
