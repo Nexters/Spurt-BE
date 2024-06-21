29e4cd253f4def3238aecdb144982ed6c707e374
emost22 <emoney96@naver.com>
2024-06-22 01:25:41 +0900

    Test: add update jobgroup

--- src/test/java/com/sirius/spurt/store/provider/jobgroup/impl/JobGroupProviderImplTest.java
+++ src/test/java/com/sirius/spurt/store/provider/jobgroup/impl/JobGroupProviderImplTest.java
@@ -1,10 +1,16 @@
 package com.sirius.spurt.store.provider.jobgroup.impl;
 
+import static com.sirius.spurt.common.meta.ResultCode.NOT_EXIST_USER;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.sirius.spurt.common.exception.GlobalException;
 import com.sirius.spurt.store.repository.database.repository.UserRepository;
 import com.sirius.spurt.test.UserTest;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -26,5 +32,39 @@ class JobGroupProviderImplTest implements UserTest {
 
         // then
         verify(userRepository).save(any());
+    }
+
+    @Nested
+    class 직군_수정 {
+        @Test
+        void 직군_수정_성공_테스트() {
+            // given
+            when(userRepository.findByUserId(any())).thenReturn(TEST_USER);
+
+            // when
+            jobGroupProvider.updateJobGroup(TEST_USER_ID, TEST_EMAIL, TEST_JOB_GROUP);
+
+            // then
+            verify(userRepository).findByUserId(any());
+            verify(userRepository).save(any());
+        }
+
+        @Test
+        void 직군_수정_실패_테스트() {
+            // given
+            when(userRepository.findByUserId(any())).thenReturn(null);
+
+            // when
+            GlobalException exception =
+                    assertThrows(
+                            GlobalException.class,
+                            () -> {
+                                jobGroupProvider.updateJobGroup(TEST_USER_ID, TEST_EMAIL, TEST_JOB_GROUP);
+                            });
+
+            // then
+            verify(userRepository).findByUserId(any());
+            assertThat(exception.getResultCode()).isEqualTo(NOT_EXIST_USER);
+        }
     }
 }
