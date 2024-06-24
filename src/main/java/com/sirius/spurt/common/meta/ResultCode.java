1f87d173d1e84f5041c3f4912d64f28802e82c10
emost22 <emoney96@naver.com>
2024-06-24 22:32:05 +0900

    Docs: add error code

--- src/main/java/com/sirius/spurt/common/meta/ResultCode.java
+++ src/main/java/com/sirius/spurt/common/meta/ResultCode.java
@@ -2,7 +2,7 @@ package com.sirius.spurt.common.meta;
 
 public enum ResultCode {
     SUCCESS(0, "정상 처리 되었습니다"),
-    SYSTEM_ERROR(1000, "알 수 없는 애러가 발생했습니다."),
+    SYSTEM_ERROR(1000, "알 수 없는 에러가 발생했습니다."),
     AUTHENTICATION_FAILED(2000, "인증에 실패했습니다."),
     NOT_QUESTION_OWNER(3000, "질문이 존재하지 않거나 작성자가 아닙니다."),
     NOT_EXIST_USER(3001, "존재하지 않는 유저입니다."),
