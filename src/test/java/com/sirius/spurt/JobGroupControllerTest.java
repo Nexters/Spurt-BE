889a8756141feb09ef77f4d6dae0c011f4da6cf8
emost22 <emoney96@naver.com>
2024-06-26 00:01:30 +0900

    Test: fix jobgroup docs

--- src/test/java/com/sirius/spurt/JobGroupControllerTest.java
+++ src/test/java/com/sirius/spurt/JobGroupControllerTest.java
@@ -1,5 +1,6 @@
 package com.sirius.spurt;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -29,7 +30,7 @@ public class JobGroupControllerTest extends BaseMvcTest {
                         .loginUser(loginUser)
                         .jobGroup(JobGroup.DEVELOPER)
                         .build();
-        when(saveJobGroupBusiness.execute(dto)).thenReturn(new SaveJobGroupBusiness.Result());
+        when(saveJobGroupBusiness.execute(any())).thenReturn(new SaveJobGroupBusiness.Result());
         this.mockMvc
                 .perform(
                         post("/v1/jobgroup")
@@ -48,7 +49,7 @@ public class JobGroupControllerTest extends BaseMvcTest {
                         .loginUser(loginUser)
                         .jobGroup(JobGroup.DEVELOPER)
                         .build();
-        when(updateJobGroupBusiness.execute(dto)).thenReturn(new UpdateJobGroupBusiness.Result());
+        when(updateJobGroupBusiness.execute(any())).thenReturn(new UpdateJobGroupBusiness.Result());
         this.mockMvc
                 .perform(
                         put("/v1/jobgroup")
