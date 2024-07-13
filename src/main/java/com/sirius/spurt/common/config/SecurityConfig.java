1d57430fd94d1201bfc661e1f121f4c894041ba8
emost22 <emoney96@naver.com>
2024-07-13 21:56:11 +0900

    Feat: add security config

---
+++ src/main/java/com/sirius/spurt/common/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.sirius.spurt.common.config;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sirius.spurt.common.filter.ExceptionHandlerFilter;
+import com.sirius.spurt.common.jwt.AuthorizationFilter;
+import com.sirius.spurt.common.jwt.JwtUtils;
+import com.sirius.spurt.store.repository.database.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final UserRepository userRepository;
+    private final JwtUtils jwtUtils;
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .logout(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+                        (sessionManagement) -> sessionManagement.sessionCreationPolicy(STATELESS))
+                .authorizeHttpRequests(
+                        (requests) ->
+                                requests
+                                        .requestMatchers("/v1/question/random")
+                                        .permitAll()
+                                        .requestMatchers("/error")
+                                        .permitAll()
+                                        .anyRequest()
+                                        .authenticated())
+                .addFilterBefore(authorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(exceptionHandlerFilter(), LogoutFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthorizationFilter authorizationFilter() {
+        return new AuthorizationFilter(userRepository, jwtUtils);
+    }
+
+    @Bean
+    public ExceptionHandlerFilter exceptionHandlerFilter() {
+        return new ExceptionHandlerFilter(objectMapper);
+    }
+}
