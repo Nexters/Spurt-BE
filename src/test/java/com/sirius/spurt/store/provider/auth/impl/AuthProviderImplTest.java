88f49c36cf1114340ffc7be02e62ca85d64c6cd2
emost22 <emoney96@naver.com>
2024-07-14 21:12:49 +0900

    Test: add save refreshToken provider

--- src/test/java/com/sirius/spurt/store/provider/auth/impl/AuthProviderImplTest.java
+++ src/test/java/com/sirius/spurt/store/provider/auth/impl/AuthProviderImplTest.java
@@ -1,7 +1,12 @@
 package com.sirius.spurt.store.provider.auth.impl;
 
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+
 import com.sirius.spurt.store.repository.redis.auth.AuthRepository;
 import com.sirius.spurt.test.TokenTest;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -12,4 +17,15 @@ class AuthProviderImplTest implements TokenTest {
     @InjectMocks private AuthProviderImpl authProvider;
 
     @Mock private AuthRepository authRepository;
+
+    @Test
+    void refreshToken_저장_테스트() {
+        // given
+
+        // when
+        authProvider.setRefreshToken(TEST_TOKEN_KEY, TEST_TOKEN_VALUE, TEST_TOKEN_EXPIRE_TIME);
+
+        // then
+        verify(authRepository).setRefreshToken(anyString(), anyString(), anyLong());
+    }
 }
